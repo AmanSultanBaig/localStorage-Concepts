484e97185f00f93f6d3d6bcb6864bd5ff5a58fba
Aman Sultan Baig <amansultanbaig@gmail.com>
2020-01-26 18:17:02 +0500

    remove specific value from localstorage

--- localStorage.js
+++ localStorage.js
@@ -6,3 +6,8 @@ localStorage.setItem('Key','Value');
 // How to access value from localStorage
 // localStorage.getItem()
 localStorage.getItem('Key'); // access by the key which you set while created value in localStorage
+
+// How to remove any specific value from localStoage 
+// localStorage.removeItem()
+localStorage.removeItem('Key');
+
