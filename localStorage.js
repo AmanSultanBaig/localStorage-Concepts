c6088e0fc8d42cb0ea303727616e497ed438a694
Aman Sultan Baig <amansultanbaig@gmail.com>
2020-01-26 18:16:21 +0500

    get item from localstorage

--- localStorage.js
+++ localStorage.js
@@ -2,3 +2,7 @@
 // how to set value in localStorage
 // localStoage.setItem()
 localStorage.setItem('Key','Value');
+
+// How to access value from localStorage
+// localStorage.getItem()
+localStorage.getItem('Key'); // access by the key which you set while created value in localStorage
